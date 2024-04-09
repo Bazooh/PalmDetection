c476e17692a60c933fb7e18c30e6f84df9aa3218
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-04-10 09:51:15 +1000

    model

---
+++ main.py
@@ -0,0 +1,9 @@
+from GUI import GUI
+from Camera import Camera
+
+def main():
+    camera = Camera()
+    gui = GUI(camera)  # Pass the camera object to the GUI
+
+if __name__ == "__main__":
+    main()
